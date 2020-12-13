3942cf74e67ff071f524eed3dc54ca0723539556
lbw3973 <lbw3973@naver.com>
2020-12-13 21:53:11 +0900

    LBW
    Please

--- app/src/main/java/com/example/cloudmemo/MainActivity.kt
+++ app/src/main/java/com/example/cloudmemo/MainActivity.kt
@@ -4,8 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.size
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        tv_count.setText(lv_Main.childCount.toString() + "개의 메모")
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            android.R.id.home ->{
+                finish()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+
+        }
+    }
+
+
+
 
 
 }
