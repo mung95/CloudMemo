d05e705a67917a04545ae928ba2be192214f073a
mung95 <mung95@naver.com>
2020-12-13 21:16:32 +0900

    mcJo
    Create Memo UI 추가

---
+++ app/src/main/java/com/example/cloudmemo/CreateMemo.kt
@@ -0,0 +1,35 @@
+package com.example.cloudmemo
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import android.view.Menu
+import android.view.MenuItem
+
+class CreateMemo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_memo)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_create_memo,menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
