1f52a1a0bbabca6574b2fe0b52e962109268c830
lbw <lbw3973@naver.com>
2020-12-19 17:35:55 +0900

    LBW
    Search View에 Button추가 (X, 검색)

--- app/src/main/java/com/example/cloudmemo/Search.kt
+++ app/src/main/java/com/example/cloudmemo/Search.kt
@@ -2,10 +2,25 @@ package com.example.cloudmemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class Search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            android.R.id.home ->{
+                finish()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+
+        }
     }
 }
